ba25dbc0e6f4480758375f91f1d7a2a2f892e479
Devin Turner <devinturner@me.com>
2025-07-31 18:41:20 -0400

    refactor: make customer phone number optional

--- api/schemas/customers.py
+++ api/schemas/customers.py
@@ -5,7 +5,7 @@ from typing import Optional
 class CustomerBase(BaseModel):
     customer_name: str
     customer_email: Optional[str] = None
-    customer_phone: int
+    customer_phone: Optional[int] = None
     customer_address: Optional[str] = None
 
 
