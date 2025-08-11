3d9198d93adbfeed9e70c3b1bb5f03041062fd1e
Devin Turner <devinturner@me.com>
2025-08-11 11:14:13 -0400

    feat(ui): add toast component for alerts

--- apps/customer-web/src/App.tsx
+++ apps/customer-web/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Toaster } from "@/components/ui/sonner"
 import './App.css'
 
 // interface {
@@ -46,14 +44,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+            <Toaster richColors position="top-center" />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
