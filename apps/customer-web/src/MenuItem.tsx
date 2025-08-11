5df7ffe4200aeeec6b895ae3328cc407173f8466
Devin Turner <devinturner@me.com>
2025-08-11 12:40:33 -0400

    feat(ui): add menu item placeholder

---
+++ apps/customer-web/src/MenuItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "./components/ui/card";
+
+const MenuItem: React.FC = () => {
+    return (
+        <Card className="w-56 h-40 p-2 flex items-center gap-3 relative overflow-hidden">
+
+        </Card>
+    );
+};
+
+export default MenuItem;
