beaf4b76715bdd3b248eaf9cb7ba1a0f8473925e
Devin Turner <devinturner@me.com>
2025-08-11 11:44:00 -0400

    feat(cart): add cartitem placeholder

--- apps/customer-web/src/Cart.tsx
+++ apps/customer-web/src/Cart.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CartItem from './CartItem';
 
 interface CartProps {
     onCheckout: () => void;
@@ -8,7 +9,10 @@ const Cart: React.FC<CartProps> = () => {
 
     return (
         <div className='w-96 flex flex-col justify-start'>
-
+                    <CartItem
+                        key={item.id}
+                        item={item}
+                    />
         </div>
     );
 };
---
+++ apps/customer-web/src/CartItem.tsx
@@ -0,0 +1,11 @@
+
+const CartItem: React.FC = () => {
+
+    return (
+        <div className="border-b px-2 pt-4 pb-2 flex flex-col justify-start gap-4">
+
+        </div>
+    );
+};
+
+export default CartItem;
