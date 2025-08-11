9747b38c0e52e4821593e2acbd4a5854c8f2f893
Devin Turner <devinturner@me.com>
2025-08-11 12:42:47 -0400

    feat(ui): add menu placeholder

---
+++ apps/customer-web/src/Menu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+
+const Menu: React.FC = () => {
+
+    return (
+        <div className="p-4">
+            <MenuItem/>
+        </div>
+    );
+};
+
+export default Menu;
