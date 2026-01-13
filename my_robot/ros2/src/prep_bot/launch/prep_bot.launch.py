7e406209d18aa1460c723c76d38c687ef9eb6320
Devin Turner <devinturner@me.com>
2026-01-13 09:39:09 -0500

    feat: add launch file and rviz setup

---
+++ my_robot/ros2/src/prep_bot/launch/prep_bot.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('prep_bot')
+    rviz_config = os.path.join(pkg_share, 'rviz', 'camera.rviz')
+    
+    return LaunchDescription([
+        Node(
+            package='prep_bot',
+            executable='record3d_bridge',
+            name='record3d_bridge',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config]
+        )
+    ])
--- my_robot/ros2/src/prep_bot/setup.py
+++ my_robot/ros2/src/prep_bot/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'prep_bot'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        ('share/' + package_name + '/rviz', ['rviz/camera.rviz']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +28,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'record3d_bridge = prep_bot.record3d_bridge:main',
         ],
     },
 )
