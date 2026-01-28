874709d53016a6ebfe3c73597fd95c2f7d734270
Devin Turner <devinturner@me.com>
2026-01-28 11:56:11 -0500

    feat: remove the promotions feature

--- services/staff-service/src/services/staff.py
+++ services/staff-service/src/services/staff.py
@@ -5,7 +5,6 @@ from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime, date
 from ..models.recipes import Recipe
 from ..models.ingredients import Ingredient
-from ..models import promotions as promotion_model
 from ..models.payment_method import Payment, PaymentStatus
 from shared.models.menu_items import MenuItem
 from shared.utils.error_handlers import handle_database_error
@@ -21,42 +20,6 @@ def get_required_ingredients(db, menu_item_id, quantity):
         ingredient.ingredient.name: ingredient.amount * quantity
         for ingredient in ingredients
     }
-
-# staff actions for promotions
-def get_promotion_by_code(db: Session, promo_code: str):
-    try:
-        item = db.query(promotion_model.Promotion).filter(promotion_model.Promotion.code == promo_code).first()
-        if not item:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Promo code not found!")
-    except SQLAlchemyError as e:
-        raise handle_database_error(e, operation="get promotion by code", log_context={"promo_code": promo_code})
-    return item
-
-
-def update_promotion_by_code(db: Session, promo_code: str, request):
-    try:
-        item = db.query(promotion_model.Promotion).filter(promotion_model.Promotion.code == promo_code)
-        if not item.first():
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Promo code not found!")
-        update_data = request.dict(exclude_unset=True)
-        item.update(update_data, synchronize_session=False)
-        db.commit()
-    except SQLAlchemyError as e:
-        raise handle_database_error(e, operation="update promotion by code", log_context={"promo_code": promo_code})
-    return item.first()
-
-
-def delete_promotion_by_code(db: Session, promo_code: str):
-    try:
-        item = db.query(promotion_model.Promotion).filter(promotion_model.Promotion.code == promo_code)
-        if not item.first():
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Promo code not found!")
-        item.delete(synchronize_session=False)
-        db.commit()
-    except SQLAlchemyError as e:
-        raise handle_database_error(e, operation="delete promotion by code", log_context={"promo_code": promo_code})
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
 
 def get_daily_revenue(db: Session, target_date: date):
     """
